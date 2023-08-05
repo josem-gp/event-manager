911e979b1db995cbde4c8a176d24b51c66510a46
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2023-08-05 23:11:14 +0900

    fix: updated syntax to comply with rubocop guidelines

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
-  def edit
-  end
+  def edit; end
 
   def update
     if current_user.update(user_params)
       redirect_to root_path, notice: t('users.successful_update')
     else
-      set_flash_errors(current_user)
+      flash_errors(current_user)
       render :edit, status: :unprocessable_entity, content_type: "text/html"
     end
   end
--- lib/utils/error_handler.rb
+++ lib/utils/error_handler.rb
@@ -1,6 +1,6 @@
 module Utils
   module ErrorHandler
-    def set_flash_errors(object)
+    def flash_errors(object)
       flash.now[:alert] = object.errors.full_messages
     end
   end
