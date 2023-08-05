b04f4d3393b33c5ce9ce3b26e88c4ac97d365ff2
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2023-08-05 21:01:52 +0900

    fix: updated syntax to comply with rubocop guidelines

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     user = User.from_omniauth(auth)
     if user.present?
       session[:user_id] = user.id
-      redirect_to root_path, notice: 'Logged in!'
+      redirect_to root_path, notice: t('sessions.logged_in')
     else
       redirect_to new_session_path, notice: "#{auth.info.email} is not authorized."
     end
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: 'Logged out!'
+    redirect_to root_path, notice: t('sessions.logged_out')
   end
 
   private
