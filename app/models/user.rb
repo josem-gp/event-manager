953d43148e21fea1f2f0d325d03cc22a75603287
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2023-08-05 18:45:16 +0900

    feat: added validations and logic to create user via omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,28 @@
 class User < ApplicationRecord
+  ALPHABETIC_REGEX = /\A[a-zA-Z]+\z/
+
   # Associations
   has_many :events_as_creator, foreign_key: :creator_id, class_name: "Event", inverse_of: :creator, dependent: :destroy
   has_many :invitees, dependent: :destroy
   has_many :events, through: :invitees
   has_many :invitations_as_sender, foreign_key: :sender_id, inverse_of: :sender, dependent: :destroy
   has_many :invitations_as_recipient, foreign_key: :recipient_id, inverse_of: :recipient, dependent: :nullify
+
+  # Validations
+  validates :email, presence: true, uniqueness: true
+  ## Starting version 7.1 and above, Rails will raise an error if you try to modify a readonly attribute
+  ## https://github.com/rails/rails/pull/46105?ref=akshaykhot.com
+  attr_readonly :email
+  validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, format: { with: ALPHABETIC_REGEX, message: :alphabetic_format }, on: :update
+
+  # If user does not exist, create it
+  def self.from_omniauth(auth)
+    where(uid: auth.uid).first_or_create do |user|
+      user.uid = auth.uid
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+      user.email = auth.info.email
+    end
+  end
 end
