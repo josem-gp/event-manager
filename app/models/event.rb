38fc594ebfc07cf144b794d2411014ec68cc81af
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2023-08-06 15:03:52 +0900

    feat: created a dashboard divided by days of the current week and displayed events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
   # Associations
   belongs_to :creator, class_name: "User", inverse_of: :created_events
+  has_many :invitees, dependent: :destroy
+  has_many :users, through: :invitees
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Rails.logger.debug "Destroying Invitations..."
+Invitation.destroy_all
+
+Rails.logger.debug "Destroying Invitees..."
+Invitee.destroy_all
+
+Rails.logger.debug "Destroying Events..."
+Event.destroy_all
+
+Rails.logger.debug "Destroying Users..."
+User.destroy_all
+
+Rails.logger.debug "Creating Users"
+5.times do
+  User.create!(
+    email: Faker::Omniauth.google[:info][:email],
+    first_name: Faker::Omniauth.google[:info][:first_name],
+    last_name: Faker::Omniauth.google[:info][:last_name],
+    password: '123456'
+  )
+end
+
+users = User.all
+
+Rails.logger.debug "Creating Events..."
+20.times do
+  rand_date = ((Date.current - 10) + rand((Date.current + 30) - (Date.current - 10)))
+
+  Event.create!(
+    title: Faker::Lorem.sentence(word_count: rand(3..8)),
+    description: Faker::Lorem.paragraph,
+    date: rand_date,
+    time: 60,
+    creator: users.sample
+  )
+end
+
+Rails.logger.debug "Creating Invitees..."
+25.times do
+  user = User.order("RANDOM()").first
+  event = Event.where.not(creator: user).order("RANDOM()").first
+  Invitee.create!(
+    user:,
+    event:
+  )
+end
+
+Rails.logger.debug "Finished seeding!"
