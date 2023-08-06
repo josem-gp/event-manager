8002b8193e11b3ec15a766fc5baf45cfa9e70ebd
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2023-08-06 12:37:55 +0900

    fix: updated syntax according to rubocop guidelines

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,9 +6,9 @@ module Users
     # before_action :configure_account_update_params, only: [:update]
 
     # GET /resource/sign_up
-    def new
-      super
-    end
+    # def new
+    #   super
+    # end
 
     # POST /resource
     def create
