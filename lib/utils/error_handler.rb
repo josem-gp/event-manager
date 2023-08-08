8ce1cd6881fd61ef14dc1324cda5dc9b6c31218a
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2023-08-09 08:36:57 +0900

    feat: improve error handling and logging throughout the project

--- lib/utils/error_handler.rb
+++ lib/utils/error_handler.rb
@@ -1,7 +1,16 @@
 module Utils
   module ErrorHandler
-    def flash_errors(object)
-      flash.now[:alert] = object.errors.full_messages
+    def flash_errors(resource)
+      flash.now[:alert] = resource.errors.full_messages
+    end
+
+    def handle_resource_error(resource, message)
+      error_message = resource.errors.full_messages.join(', ')
+      log_error(StandardError.new(error_message), message)
+    end
+
+    def log_error(error, message)
+      logger.error("#{error.class.name.demodulize} - #{message}: #{error.message}")
     end
 
     def logger
