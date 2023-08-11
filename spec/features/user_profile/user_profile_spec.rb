49924642a9350c3b750d8adc0a4bda84a94c9ba3
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2023-08-12 06:25:36 +0900

    feat: added feature tests for user profile update

---
+++ spec/features/user_profile/user_profile_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature 'User Profile Updates', type: :feature do
+  let(:user) { create(:user) }
+
+  scenario 'User can update their first name and last name' do
+    login_as(user)
+    visit edit_user_path(user)
+
+    new_first_name = 'NewFirstName'
+    new_last_name = 'NewLastName'
+
+    fill_in 'First name', with: new_first_name
+    fill_in 'Last name', with: new_last_name
+    click_button 'Submit'
+
+    expect(page).to have_current_path(root_path)
+    expect(page).to have_content("Welcome #{new_first_name} #{new_last_name}")
+  end
+
+  scenario 'User profile form guards against invalid inputs' do
+    login_as(user)
+    visit edit_user_path(user)
+
+    fill_in 'First name', with: ''
+    fill_in 'Last name', with: ''
+    click_button 'Submit'
+
+    expect(page).to have_content("First name can't be blank")
+    expect(page).to have_content("Last name can't be blank")
+  end
+end
